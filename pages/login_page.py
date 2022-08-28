15a416a09f4a1f194fd4e9e4bde9d2002ab128da
Falguni Khandelwal <falgunik28@gmail.com>
2022-08-27 21:54:37 -0400

    com

--- pages/login_page.py
+++ pages/login_page.py
@@ -5,4 +5,7 @@ class LoginPage:
     login_button = (By.ID, "login2")
     username = (By.ID, "loginusername")
     pwd = (By.ID, "loginpassword")
-    sign_in_button=(By.CSS_SELECTOR,"#logInModal .btn-primary")
+    sign_in_button = (By.CSS_SELECTOR, "#logInModal .btn-primary")
+
+
+#hjdhs
